3bf06ed6bac96884fec4a1072931a86244f001ec
AgneBvdB <Agne.banzeryte@gmail.com>
2025-07-09 15:19:30 +0200

    2017 imgs added, responsive navbar added

--- script.js
+++ script.js
@@ -118,13 +118,15 @@ window.addEventListener("resize", renderGallery);
 
 // navbar toggle
 
-function navbarToggle() {
-    const navBar = document.getElementById("myLinks");
-    if (navBar.style.display === "block") {
-        navBar.style.display = "none";
-    } else {
-        navBar.style.display = "block";
-    }
-};
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
