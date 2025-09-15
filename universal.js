16efc45d46c091f63bbc2cbc2ff6cd9d97e7814a
AgneBvdB <Agne.banzeryte@gmail.com>
2025-09-15 12:59:43 +0200

    contact form update

--- universal.js
+++ universal.js
@@ -13,3 +13,32 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }))
 });
+
+// EmailJs initialize
+
+(function(){
+    emailjs.init("o7om_pdzoxw9QgdCE");
+})();
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const serviceID = "service_l242rda";
+    const templateID = "template_gl1eduq";
+
+    const templateParams = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send(serviceID, templateID, templateParams).then(() => {
+        alert("Ačiū, žinutė išsiųsta!");
+        form.reset();
+    }, (err) => {
+        alert("O ne! Nepavyko... Bandyk iš naujo.");
+        console.error(err);
+    });
+});
